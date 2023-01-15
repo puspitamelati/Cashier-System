99daa7e624d7b84832822fb4d129abaa7bc545a8
Puspita <puspitaanggraeni769@gmail.com>
2023-01-15 11:06:25 +0700

    add file main.py, app.py, and menu.py

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ from core.app import App
 from core.menu import Menu
 
 class Transaction :
-  def __init__(self) :
-    self.app = App()
-    self.menu = Menu()
+    def __init__(self) :
+        self.app = App()
+        self.menu = Menu()
     
-  def interface (self) :
-    print('\n1.Add Item\n2. Update Item Name\n3. Update Item Quantity\n4. Update Item Harga\n')
-    user = input('Choose your transaction :')
-    print('////////////////////////////////')
-    self.menu.get_menu(user)
+    def interface (self) :
+        print('\n1.Add Item\n2. Update Item Name\n3. Update Item Quantity\n4. Update Item Harga\n')
+        user = input('Choose your transaction :')
+        print('////////////////////////////////')
+        self.menu.get_menu(user)
 
 
 m = Transaction()
